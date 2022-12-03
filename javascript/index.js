22b35b5e32bb9aa9550de3b0cc43b1b05f5685fb
Winnie <tzuhsuan1999@gmail.com>
2022-12-04 02:16:02 +0800

    修改環境

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,132 @@
-const baseUrl="https://json-server-vercel-ebon.vercel.app/";
+const baseUrl = "https://json-server-vercel-ebon.vercel.app/";
 //首頁
 //初始化畫面
-function init(){
+function init() {
   getHotSalesProductsData();
 }
 init();
 
+//取得當前日期(年-月-日)
+let Today = new Date(); //Sun Dec 04 2022 00:26:30 GMT+0800 (台北標準時間)
+let year = Today.getFullYear(); //年
+let month = Today.getMonth() + 1; //月
+let date = Today.getDate(); //日
+let week = Today.getDay(); //星期日:0
+let currentDateStr;
+let beforeDateStr;
+function getCurrentDate() {
+  //如果是禮拜日、禮拜六則取得禮拜五的報價
+  if (week === 0) {
+    date -= 2;
+  } else if (week === 1) {
+    date -= 1;
+  }
+  let dayBeforeDate=date-1;//前一日
+  if (date.toString().length === 1) {
+    date = `0${date}`;
+    dayBeforeDate=`0${dayBeforeDate}`;
+  } else {
+    return;
+  }
+  currentDateStr = `${year}-${month}-${date}`;
+  beforeDateStr=`${year}-${month}-${dayBeforeDate}`;  
+  getCurrentGoldPriceData(currentDateStr);
+  getBeforeExchangeRate(beforeDateStr);
+}
+getCurrentDate();
+
+//取得當日金價(美元)
+let currentGoldPriceData;
+let currentGoldPrice="";
+function getCurrentGoldPriceData() {
+  axios
+    .get(
+      `https://api.finmindtrade.com/api/v4/data?dataset=GoldPrice&start_date=${currentDateStr}&end_date=${currentDateStr}`
+    )
+    .then(function (response) {
+      currentGoldPriceData = response.data.data;
+      //如果當天報價異常
+      if (
+        currentGoldPriceData.length === 1 &&
+        currentGoldPriceData[0].Price > 7
+      ) {
+        console.log("報價異常");
+        getNearGoldPriceData();
+      }else{
+        currentGoldPriceData.forEach(function (item, index) {
+          let targetTime=`${beforeDateStr} 09:00:00`;
+          if (item.date === targetTime) {
+            currentGoldPrice = item.Price;
+            renderGoldPrice(currentGoldPrice);
+          }
+        });
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+//取得前日金價(美元)
+function getNearGoldPriceData() {
+  axios
+    .get(
+      `https://api.finmindtrade.com/api/v4/data?dataset=GoldPrice&start_date=${beforeDateStr}&end_date=${beforeDateStr}`
+    )
+    .then(function (response) {
+      currentGoldPriceData = response.data.data;
+      currentGoldPriceData.forEach(function (item, index) {
+        let targetTime=`${beforeDateStr} 09:00:00`;
+        if (item.date === targetTime) {
+          currentGoldPrice = item.Price;
+          renderGoldPrice(currentGoldPrice,exchangeRate);
+        }
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+let exchangeRate;
+//取得當前的美金兌換台幣匯率
+function getBeforeExchangeRate(beforeDateStr){
+  axios.get("https://api.finmindtrade.com/api/v3/data?dataset=TaiwanExchangeRate&data_id=USD&date=2022-12-01")
+  .then(function(response){
+    exchangeRate=response.data.data;
+    exchangeRate.forEach(function(item){
+      if(item.date===beforeDateStr){
+        exchangeRate=item.spot_buy;        
+        renderGoldPrice(currentGoldPrice,exchangeRate);
+      }
+    })
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+}
+
+//渲染當前金價
+const test=document.querySelector(".render-test");
+console.log(test)
+function renderGoldPrice(price,exchangeRate){
+  // console.log(price,exchangeRate)
+  console.log(price*exchangeRate)
+  let GoldPrice=((price*exchangeRate/8.29426).toFixed()/10).toFixed()*10;
+  test.textContent=GoldPrice;
+}
+
+
 
 //熱銷商品
-const hotSalesProductsList=document.querySelector(".hotSales-products-list");
+const hotSalesProductsList = document.querySelector(".hotSales-products-list");
 let hotSalesProductsData;
 //取得熱銷商品資料
 function getHotSalesProductsData() {
   axios
     .get(`${baseUrl}hotSalesProducts`)
     .then(function (response) {
-      hotSalesProductsData=response.data;
+      hotSalesProductsData = response.data;
       renderHotSalesProductsData();
     })
     .catch(function (error) {
@@ -24,10 +135,10 @@ function getHotSalesProductsData() {
 }
 
 //渲染熱銷商品畫面
-function renderHotSalesProductsData(){
-  let str="";
-  hotSalesProductsData.forEach(function(item){
-    str+=`<div class="swiper-slide">
+function renderHotSalesProductsData() {
+  let str = "";
+  hotSalesProductsData.forEach(function (item) {
+    str += `<div class="swiper-slide">
     <div>
       <div
         class="rounded-3 mb-3 swiper-img"
@@ -76,8 +187,8 @@ function renderHotSalesProductsData(){
       </div>
     </div>
   </div>`;
-  })
-  hotSalesProductsList.innerHTML=str;
+  });
+  hotSalesProductsList.innerHTML = str;
 }
 
 //swiper
@@ -105,8 +216,6 @@ var swiper = new Swiper(".hot-products .mySwiper", {
     },
   },
 });
-
-
 
 //熱門分類
 //swiper
