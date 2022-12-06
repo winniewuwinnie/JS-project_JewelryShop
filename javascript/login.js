30bbec677d6b7f4cfe4973191515723ae110f4ff
Winnie <tzuhsuan1999@gmail.com>
2022-12-07 01:07:47 +0800

    修正環境

--- javascript/login.js
+++ javascript/login.js
@@ -1,28 +1,10 @@
+
+const baseUrl = "https://json-server-vercel-ebon.vercel.app/";
+// const baseUrl = "http://localhost:3000/";
+
 //validate
 let constraints = {
   帳號名稱: {
-    presence: {
-      message: "必填",
-    },
-  },
-  電子信箱: {
-    presence: {
-      message: "必填",
-    },
-    email: {
-      message: "格式有誤",
-    },
-  },
-  聯絡電話: {
-    presence: {
-      message: "必填",
-    },
-    length: {
-      minimum: 8,
-      message: "號碼需超過 8 碼",
-    },
-  },
-  通訊地址: {
     presence: {
       message: "必填",
     },
@@ -33,17 +15,33 @@ let constraints = {
     },
     length: {
       minimum: 6,
-      message: "帳號密碼長度須至少6個字元",
+      message: "至少6個字元",
     },
   },
 };
 const form=document.querySelector("form");
 const username=document.querySelector(".username");
-const email=document.querySelector(".tel");
-const address=document.querySelector(".address");
 const password=document.querySelector(".password");
-const checkPassword=document.querySelector(".checkPassword");
-form.addEventListener("submit",function(e){
+const errorMessage=document.querySelectorAll(".error-message");
+const loginBtn=document.querySelector(".login-btn");
+loginBtn.addEventListener("click",function(e){
   e.preventDefault();
-  console.log(validate(form, constraints))
+  let error=validate(form, constraints);
+  if(error){
+    showErrors(error);
+  }else{
+
+    //比對有沒有註冊過
+    location.href="index.html"
+  }
 })
+
+function showErrors(errors) {
+  errorMessage.forEach(function (item) {
+    if(errors[item.dataset.msg]===undefined){
+      return;
+    }else if(errors[item.dataset.msg]){
+      item.innerHTML = `<i class="bi bi-exclamation-circle me-1"></i>${errors[item.dataset.msg]}`;
+    }
+  });
+}
--- javascript/register.js
+++ javascript/register.js
@@ -1,3 +1,9 @@
+
+
+const baseUrl = "https://json-server-vercel-ebon.vercel.app/";
+// const baseUrl = "http://localhost:3000/";
+
+
 //modal
 var popoverTriggerList = [].slice.call(
   document.querySelectorAll('[data-bs-toggle="popover"]')
@@ -41,17 +47,75 @@ let constraints = {
     },
     length: {
       minimum: 6,
-      message: "帳號密碼長度須至少6個字元",
+      message: "長度須至少6個字元",
+    },
+  },
+  兩次帳號密碼: {
+    presence: {
+      message: "必填",
+    },
+    equality: {
+      attribute: "帳號密碼",
+      message: "輸入不相同",
     },
   },
 };
-const form=document.querySelector("form");
-const username=document.querySelector(".username");
-const email=document.querySelector(".tel");
-const address=document.querySelector(".address");
-const password=document.querySelector(".password");
-const checkPassword=document.querySelector(".checkPassword");
-form.addEventListener("submit",function(e){
+const form = document.querySelector("form");
+const username = document.querySelector(".username");
+const email = document.querySelector(".email");
+const tel=document.querySelector(".tel");
+const address = document.querySelector(".address");
+const password = document.querySelector(".password");
+const checkPassword = document.querySelector(".checkPassword");
+const registerBtn=document.querySelector(".register-btn");
+const errorMessage = document.querySelectorAll(".error-message");
+
+registerBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log(validate(form, constraints))
-})
+  let error = validate(form, constraints);
+  if (error) {
+    showErrors(error);
+  } else {
+    let userData={
+      "username":username.value.trim(),
+      "email":email.value.trim(),
+      "tel":tel.value.trim(),
+      "address":address.value.trim(),
+      "password":password.value.trim()
+    }
+    addUser(userData);
+  }
+});
+
+function showErrors(errors) {
+  errorMessage.forEach(function (item) {
+    if (errors[item.dataset.msg] === undefined) {
+      return;
+    } else if (errors[item.dataset.msg]) {
+      item.innerHTML = `<i class="bi bi-exclamation-circle me-1"></i>${
+        errors[item.dataset.msg]
+      }`;
+    }
+  });
+}
+
+function addUser(userData){
+  axios.post(`${baseUrl}users`,userData)
+  .then(function(response){
+    location.href="login.html";
+    alert("註冊成功！")
+    // if(response.status===201){
+    //   console.log(response)
+    //   alert("註冊成功！")
+    //   location.href="login.html"
+    // }
+  })
+  .catch(function(error){
+    console.log(error.response)
+    if(error.response.status===400){
+      if(error.response.data==="Email already exists"){
+        alert("此電子信箱已被註冊過")
+      }
+    };
+  })
+}
