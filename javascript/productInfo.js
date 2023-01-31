4615debad0709ed45ac7d5f9a223700175b9f808
Winnie <tzuhsuan1999@gmail.com>
2023-01-31 13:48:43 +0800

    修正環境

--- javascript/productInfo.js
+++ javascript/productInfo.js
@@ -1,5 +1,5 @@
-// const baseUrl = "https://json-server-vercel-teal-seven.vercel.app/";
-const baseUrl = "http://localhost:3000/";
+const baseUrl = "https://json-server-vercel-teal-seven.vercel.app/";
+// const baseUrl = "http://localhost:3000/";
 
 let productId = location.href.split("=")[1];
 let userId=parseInt(localStorage.getItem("userId"));
